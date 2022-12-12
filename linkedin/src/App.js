cc91696d4c828d2c02daa4857a981c5be3c6505d
DrPleasure <114397562+DrPleasure@users.noreply.github.com>
2022-12-12 18:29:44 +0100

    added contactpage

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProfilePage from "./components/ProfilePage";
 import NotFound from "./components/NotFound"
+import Contact from "./components/Contact";
 function App() {
   return (
        
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter> 
       <Routes>
       <Route path="/" element={<ProfilePage />} />
+      <Route path="/contact" element={<Contact />} />
        <Route path="*" element={<NotFound />} /> 
       </Routes>
       </BrowserRouter>
---
+++ linkedin/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Contact
+() {
+  return (
+    <div>
+    <h1>Find us here:</h1>
+    <h3>Poland</h3>
+    <img src="https://media.istockphoto.com/id/657672856/vector/flag-of-poland.jpg?s=612x612&w=0&k=20&c=eD4TNgtod4k5yshZQeCS6OeVa4zA4b5OQm4T8TtJzD0=" alt='flag' />
+    </div>
+  )
+}
