237b2d0d014f8343035eab2395e0d0878065ae60
Damian Olszewski <damianolszewski25@gmail.com>
2022-12-12 12:03:44 +0100

    kek

--- linkedin/src/components/MyFooter.jsx
+++ linkedin/src/components/MyFooter.jsx
@@ -4,83 +4,154 @@ export default class MyFooter extends Component {
   render() {
     return (
       <Container id="footing" className="smaller mt-5">
-        <Row>
-          <Col>
-            <Navbar className="d-flex flex-column">
-              <Nav.Link className="d-flex">
+        {/* Footer */}
+        <footer className="page-footer font-small indigo">
+          {/* Footer Links */}
+          <div className="container text-center text-md-left">
+            {/* Grid row */}
+            <div className="row">
+              {/* Grid column */}
+              <div className="col-md-2 mx-auto">
+                {/* Links */}
                 <img
                   style={{ width: "8rem" }}
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/LinkedIn_2021.svg/1024px-LinkedIn_2021.svg.png"
                   alt="..."
                 />
-              </Nav.Link>
-              <Nav.Item>
-                <Nav.Link className="disabled">About</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">Community GuideLines</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">Privacy & Terms</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">Sales Solutions</Nav.Link>
-              </Nav.Item>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="#!" className="disabled">
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#!">Community Guidelines</a>
+                  </li>
+                  <li>
+                    <a href="#!">Privacy & Terms</a>
+                  </li>
+                  <li>
+                    <a href="#!">Sales Solutions</a>
+                  </li>
+                  <li>
+                    <a href="#!">Safety Center</a>
+                  </li>
+                </ul>
+              </div>
+              {/* Grid column */}
+              <hr className="clearfix w-100 d-md-none" />
+              {/* Grid column */}
+              <div className="col-md-2 mx-auto">
+                {/* Links */}
 
-              <Nav.Item>
-                <Nav.Link className="disabled">Safety Center</Nav.Link>
-              </Nav.Item>
-            </Navbar>
-          </Col>
-          <Col>
-            <Navbar className="d-flex flex-column">
-              <Nav.Item>
-                <Nav.Link className="disabled">Accessibility</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">Careers</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">Ad Choice</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">Mobile</Nav.Link>
-              </Nav.Item>
-            </Navbar>
-          </Col>
-          <Col>
-            <Navbar className="d-flex flex-column">
-              <Nav.Item>
-                <Nav.Link className="disabled">Talent Solutions</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">Marketing Solutions</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">Advertising</Nav.Link>
-              </Nav.Item>
-            </Navbar>
-          </Col>
-          <Col>
-            <Navbar className="d-flex flex-column">
-              <Nav.Item>
-                <Nav.Link className="disabled">
-                  <h2>Questions</h2>
-                  <p>Visit our Help Center</p>
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">
-                  <h2>Manage your account and privacy</h2>
-                  <p>Go to your Settings</p>
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link className="disabled">Corporate Information</Nav.Link>
-              </Nav.Item>
-            </Navbar>
-          </Col>
-        </Row>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="#!">Accessibility</a>
+                  </li>
+                  <li>
+                    <a href="#!">Careers</a>
+                  </li>
+                  <li>
+                    <a href="#!">Ad Choices</a>
+                  </li>
+                  <li>
+                    <a href="#!">Mobile</a>
+                  </li>
+                </ul>
+              </div>
+              {/* Grid column */}
+              <hr className="clearfix w-100 d-md-none" />
+              {/* Grid column */}
+              <div className="col-md-2 mx-auto">
+                {/* Links */}
+
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="#!">Talent Solutions</a>
+                  </li>
+                  <li>
+                    <a href="#!">Marketing Solutions</a>
+                  </li>
+                  <li>
+                    <a href="#!">Advertising</a>
+                  </li>
+                  <li>
+                    <a href="#!">Small Business</a>
+                  </li>
+                </ul>
+              </div>
+              {/* Grid column */}
+              <hr className="clearfix w-100 d-md-none" />
+              {/* Grid column */}
+              <div className="col-md-2 mx-auto">
+                {/* Links */}
+
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="#!">
+                      <h4>Questions?</h4>
+                    </a>
+                    <p>Visit our Help Center</p>
+                  </li>
+                  <li>
+                    <a href="#!" type="disabled">
+                      <h5>Manage your account in privacy</h5>
+                    </a>
+                    <p>Go to your Settings</p>
+                  </li>
+                </ul>
+              </div>
+              <hr className="clearfix w-100 d-md-none" />
+              {/* Grid column */}
+              <div className="col-md-2 mx-auto">
+                {/* Links */}
+
+                <ul className="list-unstyled">
+                  <li>
+                    <label for="cars">Select Language</label>
+                    <select style={{ width: "18rem" }} name="cars" id="cars">
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                      <option value="eng">English(English)</option>
+                    </select>
+                  </li>
+                </ul>
+              </div>
+              {/* Grid column */}
+            </div>
+            {/* Grid row */}
+          </div>
+          {/* Footer Links */}
+          {/* Copyright */}
+          <div className="footer-copyright text-start py-3">
+            LinkedIn Corporation © 2022
+          </div>
+          {/* Copyright */}
+        </footer>
+        {/* Footer */}
       </Container>
     );
   }
