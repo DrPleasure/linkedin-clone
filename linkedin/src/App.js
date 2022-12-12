4f46b5df78681179dcbd602f045cf8b504f033ce
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-12-12 13:36:44 +0100

    fixes

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,10 +1,11 @@
 import MyFooter from "./components/MyFooter";
 import NavbarTop from "./components/NavbarTop";
+import Profile from "./components/Profile";
 function App() {
   return (
     <div>
       <NavbarTop />
-      <Profile/>
+      <Profile />
       <MyFooter />
     </div>
   );
