f9c1266c6de4fb9fbd2998d2dd2aa74c5dfbd4f0
Damian Olszewski <damianolszewski25@gmail.com>
2022-12-14 17:25:52 +0100

    anotherone

--- linkedin/src/components/Experiences.jsx
+++ linkedin/src/components/Experiences.jsx
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 import { Modal, InputGroup, Form } from "react-bootstrap";
 import { useState } from "react";
 import { MdOutlineModeEditOutline } from "react-icons/md";
-
+import { AiOutlinePlus } from "react-icons/ai";
 const Experiences = () => {
   const dispatch = useDispatch();
   const experiences = useSelector((state) => state.user.experiences);
@@ -15,8 +15,11 @@ const Experiences = () => {
   console.log(experiences);
 
   const [show, setShow] = useState(false);
+  const [add, setAdd] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleAdd = () => setAdd(true);
+  const handleNoAdd = () => setAdd(false);
   return (
     <>
       <Modal show={show} onHide={handleClose}>
@@ -142,15 +145,116 @@ const Experiences = () => {
           </Button>
         </Modal.Footer>
       </Modal>
+      {/* 2nd Modal */}
+      <Modal show={add} onHide={handleAdd}>
+        <Modal.Header closeButton onClick={handleNoAdd}>
+          <Modal.Title>Add new experiences: </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul class="list-group">
+            <li class="list-group-item border-bottom transparent">
+              <div className="d-flex align-items-start">
+                <div>
+                  <div class="form-group row mb-1">
+                    <label for="staticEmail" class="col-sm-3 col-form-label">
+                      Company:
+                    </label>
+                    <div class="col-sm-9">
+                      <InputGroup
+                        type="text"
+                        class="form-control-plaintext"
+                        value="email@example.com"
+                      >
+                        <Form.Control placeholder="Add company name" />
+                      </InputGroup>
+                    </div>
+                  </div>
+                  <div class="form-group row mb-1">
+                    <label for="staticEmail" class="col-sm-3 col-form-label">
+                      Position:
+                    </label>
+                    <div class="col-sm-9">
+                      <InputGroup
+                        type="text"
+                        class="form-control-plaintext"
+                        value="email@example.com"
+                      >
+                        <Form.Control placeholder="Add position in company" />
+                      </InputGroup>
+                    </div>
+                  </div>
+                  <div class="form-group row mb-1">
+                    <label for="staticEmail" class="col-sm-3 col-form-label">
+                      Description:
+                    </label>
+                    <div class="col-sm-9">
+                      <InputGroup
+                        type="text"
+                        class="form-control-plaintext"
+                        value="email@example.com"
+                      >
+                        <Form.Control placeholder="Small description" />
+                      </InputGroup>
+                    </div>
+                  </div>
+                  <div class="form-group row mb-1">
+                    <label for="staticEmail" class="col-sm-3 col-form-label">
+                      Start:
+                    </label>
+                    <div class="col-sm-9">
+                      <InputGroup
+                        type="text"
+                        class="form-control-plaintext"
+                        value="email@example.com"
+                      >
+                        <Form.Control placeholder="Add time of work start" />
+                      </InputGroup>
+                    </div>
+                  </div>
+                  <div class="form-group row mb-1">
+                    <label for="staticEmail" class="col-sm-3 col-form-label">
+                      End:
+                    </label>
+                    <div class="col-sm-9">
+                      <InputGroup
+                        type="text"
+                        class="form-control-plaintext"
+                        value="email@example.com"
+                      >
+                        <Form.Control placeholder="Add time u ended work " />
+                      </InputGroup>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={handleNoAdd}
+            id="buttons-rounded"
+          >
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleNoAdd} id="buttons-rounded">
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
       <div className="bg-light mt-3 rounded p-3">
         <div className="d-flex justify-content-between">
           <h4>Experiences</h4>
-          <MdOutlineModeEditOutline
-            size={30}
-            onClick={handleShow}
-            className="hover"
-          />
+          <div>
+            <AiOutlinePlus onClick={handleAdd} size={30} className="hover" />
+            <MdOutlineModeEditOutline
+              size={30}
+              onClick={handleShow}
+              className="hover"
+            />
+          </div>
         </div>
         <ul class="list-group">
           <li class="list-group-item border-bottom transparent">
--- linkedin/src/redux/store/index.js
+++ linkedin/src/redux/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import experienceReducer from "../reducers/experienceReducer";
 import userReducer from "../reducers/userReducer";
 
 // configureStore will set up the Redux Store for us!
 
 const bigReducer = combineReducers({
   user: userReducer,
+  experience: experienceReducer,
 });
 
 const store = configureStore({
