42fce41a0c58612375f50f57bb289dfe1ca8eb22
DrPleasure <114397562+DrPleasure@users.noreply.github.com>
2022-12-15 14:50:20 +0100

    im out fukit

--- linkedin/src/redux/reducers/userReducer.js
+++ linkedin/src/redux/reducers/userReducer.js
@@ -3,15 +3,29 @@ import {
   GET_ALL_USERS,
   GET_EXPERIENCES,
   GET_POSTS,
+  OTHER_USER_DETAILS,
+  GET_EXPERIENCE_DETAILS_OTHER
 } from "../actions/actionType";
 const INITIAL_STATE = {
   user: [],
   users: [],
   experiences: [],
   posts: [],
+  otherUser: [],
+  otherExperience: []
 };
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case OTHER_USER_DETAILS:
+      return {
+        ...state,
+        otherUser: action.payload,
+      };
+      case GET_EXPERIENCE_DETAILS_OTHER:
+      return {
+        ...state,
+        otherExperience: action.payload,
+      };
     case GET_ALL_USERS:
       return {
         ...state,
